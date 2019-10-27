7b928895f18f6ce38e2672f00280461da2632db3
atorr5555 <antoniotorr5555@gmail.com>
2019-10-27 10:25:32 -0600

    Added Game

---
+++ GAME1/Assets/ControlCaminar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCaminar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public Animator abajo;
+    public Animator principal_0;
+
+    //public float Velocidad;
+   // private Rigidbody2D Body;
+  
+    void Start()
+    {
+        //Body = GetComponent<Rigidbody2D>();
+        principal_0 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-.1f, 0, 0);
+             principal_0.SetBool("camina2", true);
+          
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(.1f, 0, 0);
+            principal_0.SetBool("camina3", true);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += new Vector3(0, .1f, 0);
+            principal_0.SetBool("camina4", true);
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += new Vector3(0, -.1f, 0);
+            principal_0.SetBool("camina1", true);
+        }
+       
+    }
+}
---
+++ GAME1/Assets/Interaccion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaccion : MonoBehaviour
+{
+    public Rigidbody2D principal;
+    public Rigidbody2D alcalde;
+    public Canvas alcaldeCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        principal.GetComponent<Rigidbody2D>();
+        alcalde.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+            alcaldeCam.gameObject.SetActive(true);
+        }
+        
+    }
+}
